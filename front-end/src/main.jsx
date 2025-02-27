e383ae16207e2d74985a0f393ef187d96037c589
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2025-02-27 22:20:40 +0530

    basic setup for google authentication are done

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="845094381275-apbbb0qac239aa8c9f2kjvu08457dnq0.apps.googleusercontent.com">
+    <StrictMode>
+        <App />
+    </StrictMode>
+  </GoogleOAuthProvider>,
+  
 )
