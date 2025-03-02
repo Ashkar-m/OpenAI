ebd77e235ea9b8a01c76a1ff7c1ecea92079a236
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2025-03-02 12:37:07 +0530

    Postgres database added

--- backend/details/urls.py
+++ backend/details/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .import views
+from .views import RegisterView,LoginView
 
 urlpatterns = [
-    path('register/', views.RegisterView, name='register'),
-    path('login/', views.LoginView, name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
