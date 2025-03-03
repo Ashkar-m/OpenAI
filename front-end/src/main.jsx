819999e801543d065230f0e47e828956e3f58c54
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2025-03-03 23:03:10 +0530

    Updated main.jsx file

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="845094381275-apbbb0qac239aa8c9f2kjvu08457dnq0.apps.googleusercontent.com">
